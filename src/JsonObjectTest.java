a917c75bfb77b56124dcaf8fd2eb16c806643ca6
Robbie Talbot <Robbie Talbot>
2017-03-02 17:13:45 -0700

    Initializing repository

---
+++ src/JsonObjectTest.java
@@ -0,0 +1,17 @@
+import org.json.simple.JSONObject;
+
+class Main {
+
+    public static void main(String[] args){
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("name","Shem");
+        obj.put("num",new Integer(100));
+        obj.put("balance",new Double(1000.21));
+        obj.put("is_vip",new Boolean(true));
+        obj.put("Number_Of_Frisbees",5700);
+
+        System.out.print(obj.get("name"));
+    }
+}
